6dbbae259ab13aa49655c1a66b35511fb2c1ce51
Aman Murya <m.amanmaurya49@gmail.com>
2025-01-11 15:06:37 +0530

    added responstruct from immutable for structured response

--- Backend/controller/post.js
+++ Backend/controller/post.js
@@ -1,7 +1,25 @@
 const Post=require("../model/postSchema");
 const responseStruct=require("../helper/responseStructure");
-export const getUserPost= async (data, err)=>{
+export const getUserPost= async (data, cb)=>{
     try{
+        if(!data.type){
+            throw new Error("Params Missing");
+        }
+        const query={
+            user_id:data.req.auth.user_id,
+        }
+        const res=await Post.find(query);
+        return cb(
+            null,
+            responseStruct
+            .merge({
+                action:'get_user_posts',
+                status:200,
+                success:true,
+                message:"success",
+                data:res
+            })
+        )
 
     }
     catch(err){
